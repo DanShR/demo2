7c2fe2fea11649410bf8feb9f447d91dfece3d09
Daniyar Shulakov <dshulakov@gmail.com>
2019-11-05 13:22:16 +0600

    init

---
+++ src/main/java/com/example/demo2/Country.java
@@ -0,0 +1,32 @@
+package com.example.demo2;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "country")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public Country() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/demo2/DemoController.java
@@ -0,0 +1,30 @@
+package com.example.demo2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    private final CountryRepo countryRepo;
+
+    @Autowired
+    public DemoController(CountryRepo countryRepo) {
+        this.countryRepo = countryRepo;
+    }
+
+    @GetMapping
+    public List<Country> demo() {
+        Country country = new Country();
+        country.setName("RU");
+        countryRepo.save(country);
+        List<Country> countryList = countryRepo.findAll();
+        return countryList;
+    }
+
+}
